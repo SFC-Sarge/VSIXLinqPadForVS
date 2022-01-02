3fbd20a51e6cc9534b1c6a728a07ac596bf52623
Danny McNaught <danny.mcnaught@dannymcnaught.com>
2022-01-01 22:27:24 -0500

    Add project files.

---
+++ VSIXLinqPadForVS/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.Imaging;
+
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VSIXLinqPadForVS
+{
+    public class MyToolWindow : BaseToolWindow<MyToolWindow>
+    {
+        public override string GetTitle(int toolWindowId) => "LinqPad Dump";
+
+        public override Type PaneType => typeof(Pane);
+
+        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+        }
+
+        [Guid("23e7c5a3-6e43-41e5-81fd-bf2297047dce")]
+        internal class Pane : ToolWindowPane
+        {
+            public Pane()
+            {
+                BitmapImageMoniker = KnownMonikers.ToolWindow;
+            }
+        }
+    }
+}
---
+++ VSIXLinqPadForVS/ToolWindows/MyToolWindowControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using LINQPad;
+
+namespace VSIXLinqPadForVS
+{
+    public partial class MyToolWindowControl : UserControl
+    {
+        OutputWindowPane pane = null;
+        string[] names = { "Bob", "Ned", "Amy", "Bill" };
+      public MyToolWindowControl()
+        {
+            DoOutputWindowsAsync();
+            InitializeComponent();
+        }
+
+        private async void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await VS.MessageBox.ShowAsync("LinqPadForVS", "Button clicked");
+            await pane.WriteLineAsync($"2 * 25 = {(2 * 25).Dump<int>()}");
+            await pane.WriteLineAsync($"2 * 50 = {(2 * 50).Dump<int>()}");
+            await pane.WriteLineAsync($"2 * 75 = {(2 * 75).Dump<int>()}");
+            await pane.WriteLineAsync($"button1 was clicked: Result: {result}");
+            await pane.WriteLineAsync($"Does any of the names: {names[0]}, {names[1]}, {names[2]}, or {names[3]} StartsWith(B)? {names.Any(n => n.StartsWith("B")).Dump()}");
+        }
+        private async Task DoOutputWindowsAsync()
+        {
+            pane = await VS.Windows.CreateOutputWindowPaneAsync("LinqPad Dump");
+            return;
+        }
+    }
+}
