753de5d9950cc2e5710579c6ff70493f14caf838
Danny McNaught <danny.mcnaught@dannymcnaught.com>
2022-01-07 20:40:21 -0500

    Fixed missing Package Attributes.

--- VSIXLinqPadForVS/VSIXLinqPadForVSPackage.cs
+++ VSIXLinqPadForVS/VSIXLinqPadForVSPackage.cs
@@ -7,6 +7,8 @@ global using System;
 global using Task = System.Threading.Tasks.Task;
 
 using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
 
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -21,16 +23,46 @@ namespace VSIXLinqPadForVS
     [ProvideToolWindowVisibility(typeof(MyToolWindow.Pane), VSConstants.UICONTEXT.NoSolution_string)]
     [ProvideToolWindowVisibility(typeof(MyToolWindow.Pane), VSConstants.UICONTEXT.EmptySolution_string)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideLanguageService(typeof(LinqEditor), Constants.LanguageName, 0, ShowHotURLs = false, DefaultToNonHotURLs = true, EnableLineNumbers = true, EnableAsyncCompletion = true, EnableCommenting = true, ShowCompletion = true)]
+    [ProvideLanguageEditorOptionPage(typeof(OptionsProvider.AdvancedOptions), Constants.LanguageName, "", "Advanced", null, 0)]
+    [ProvideLanguageExtension(typeof(LinqEditor), Constants.FileExtension)]
+    [ProvideEditorExtension(typeof(LinqEditor), Constants.FileExtension, 50)]
+    [ProvideFileIcon(Constants.FileExtension, "KnownMonikers.RegistrationScript")]
+    [ProvideEditorFactory(typeof(LinqEditor), 0, false, CommonPhysicalViewAttributes = (int)__VSPHYSICALVIEWATTRIBUTES.PVA_SupportsPreview, TrustLevel = __VSEDITORTRUSTLEVEL.ETL_AlwaysTrusted)]
+    [ProvideEditorLogicalView(typeof(LinqEditor), VSConstants.LOGVIEWID.TextView_string, IsTrusted = true)]
     [ProvideService(typeof(ToolWindowMessenger), IsAsyncQueryable = true)]
     [Guid(PackageGuids.VSIXLinqPadForVSString)]
     public sealed class VSIXLinqPadForVSPackage : ToolkitPackage
     {
+        internal static LinqEditor LinqEditor { get; private set; }
+
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            LinqEditor = new(this);
+            RegisterEditorFactory(LinqEditor);
+
+            SetInternetExplorerRegistryKey();
+
             AddService(typeof(ToolWindowMessenger), (_, _, _) => Task.FromResult<object>(new ToolWindowMessenger()));
             await this.RegisterCommandsAsync();
 
             this.RegisterToolWindows();
+        }
+        // This is to enable DPI scaling in the preview browser instance
+        private static void SetInternetExplorerRegistryKey()
+        {
+            try
+            {
+                using (RegistryKey featureControl = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Internet Explorer\\Main\\FeatureControl", true))
+                using (RegistryKey pixel = featureControl.CreateSubKey("FEATURE_96DPI_PIXEL", true, RegistryOptions.Volatile))
+                {
+                    pixel.SetValue("devenv.exe", 1, RegistryValueKind.DWord);
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Log();
+            }
         }
     }
 }
